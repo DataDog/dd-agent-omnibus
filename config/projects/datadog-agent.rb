35e490bc743bd49a9ee043a70201a193d1ac1697
Remi Hakim <remi@datadoghq.com>
2015-03-17 15:09:40 -0400

    Fix logic

--- config/projects/datadog-agent.rb
+++ config/projects/datadog-agent.rb
@@ -20,13 +20,11 @@ description "Datadog Monitoring Agent
 "
 platform_in_iteration false
 
-
-
 if Ohai['platform'] == 'rhel'
     replaces "datadog-agent-base < 5.0.0"
     replaces "datadog-agent-lib < 5.0.0"
 
-if Ohai['platform'] == 'debian'
+elsif Ohai['platform'] == 'debian'
     replaces "datadog-agent-base (<< 5.0.0)"
     replaces "datadog-agent-lib (<< 5.0.0)"
     conflict "datadog-agent-base (<< 5.0.0)"
