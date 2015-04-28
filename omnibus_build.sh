4fc54dfec1e7f26e2149f2a2866e817b84e0c5a8
Remi Hakim <remi@datadoghq.com>
2015-04-28 18:14:36 +0200

    Allow to use any dd-agent-omnibus branch

--- omnibus_build.sh
+++ omnibus_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 PROJECT_DIR=dd-agent-omnibus
 PROJECT_NAME=datadog-agent
-${LOG_LEVEL:=info}
+LOG_LEVEL=${LOG_LEVEL:-"info"}
+OMNIBUS_BRANCH=${OMNIBUS_BRANCH:-"master"}
 
 # Clean up omnibus artifacts
 rm -rf /var/cache/omnibus/pkg/*
@@ -12,6 +13,9 @@ rm -rf /etc/dd-agent
 rm -rf /opt/$PROJECT_NAME/*
 
 cd $PROJECT_DIR
+# Allow to use a different dd-agent-omnibus branch
+git checkout $OMNIBUS_BRANCH 
+git pull
 # Install the gems we need, with stubs in bin/
 bundle update # Make sure to update to the latest version of omnibus-software
 bin/omnibus build -l=$LOG_LEVEL $PROJECT_NAME
