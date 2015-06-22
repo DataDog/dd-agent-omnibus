035c19afd1b8d7502c2ec6a276dee65dcd3700e9
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-06-22 10:54:20 -0400

    [Omnibus4] Always build Gohai + code cleanup

--- config/software/datadog-agent.rb
+++ config/software/datadog-agent.rb
@@ -1,58 +1,58 @@
 name 'datadog-agent'
 
-local_agent_repo = ENV["LOCAL_AGENT_REPO"]
+local_agent_repo = ENV['LOCAL_AGENT_REPO']
 if local_agent_repo.nil? || local_agent_repo.empty?
   source git: 'https://github.com/DataDog/dd-agent.git'
 else
   # For local development
-  source path: ENV["LOCAL_AGENT_REPO"]
+  source path: ENV['LOCAL_AGENT_REPO']
 end
 
-agent_branch = ENV["AGENT_BRANCH"]
+agent_branch = ENV['AGENT_BRANCH']
 if agent_branch.nil? || agent_branch.empty?
-  default_version "master"
+  default_version 'master'
 else
   default_version agent_branch
 end
 
-relative_path "dd-agent"
+relative_path 'dd-agent'
 
 build do
-  ship_license "https://raw.githubusercontent.com/DataDog/dd-agent/master/LICENSE"
+  ship_license 'https://raw.githubusercontent.com/DataDog/dd-agent/master/LICENSE'
   # Agent code
   mkdir  "#{install_dir}/agent/"
-  copy "checks.d", "#{install_dir}/agent/"
-  copy "checks", "#{install_dir}/agent/"
-  copy "dogstream", "#{install_dir}/agent/"
-  copy "resources", "#{install_dir}/agent/"
-  copy "utils", "#{install_dir}/agent/"
+  copy 'checks.d', "#{install_dir}/agent/"
+  copy 'checks', "#{install_dir}/agent/"
+  copy 'dogstream', "#{install_dir}/agent/"
+  copy 'resources', "#{install_dir}/agent/"
+  copy 'utils', "#{install_dir}/agent/"
   command "cp *.py #{install_dir}/agent/"
-  copy "datadog-cert.pem", "#{install_dir}/agent/"
+  copy 'datadog-cert.pem', "#{install_dir}/agent/"
 
   mkdir "#{install_dir}/run/"
 
   # Configuration files
-  mkdir "/etc/dd-agent"
-  if ohai["platform_family"] == "rhel"
-    copy "packaging/centos/datadog-agent.init", "/etc/init.d/datadog-agent"
-  elsif ohai["platform_family"] == "debian"
-    copy "packaging/debian/datadog-agent.init", "/etc/init.d/datadog-agent"
-    mkdir "/lib/systemd/system"
-    copy "packaging/debian/datadog-agent.service", "/lib/systemd/system/datadog-agent.service"
-    copy "packaging/debian/start_agent.sh", "/opt/datadog-agent/bin/start_agent.sh"
-    command "chmod 755 /opt/datadog-agent/bin/start_agent.sh"
+  mkdir '/etc/dd-agent'
+  if ohai['platform_family'] == 'rhel'
+    copy 'packaging/centos/datadog-agent.init', '/etc/init.d/datadog-agent'
+  elsif ohai['platform_family'] == 'debian'
+    copy 'packaging/debian/datadog-agent.init', '/etc/init.d/datadog-agent'
+    mkdir '/lib/systemd/system'
+    copy 'packaging/debian/datadog-agent.service', '/lib/systemd/system/datadog-agent.service'
+    copy 'packaging/debian/start_agent.sh', '/opt/datadog-agent/bin/start_agent.sh'
+    command 'chmod 755 /opt/datadog-agent/bin/start_agent.sh'
   end
-  copy "packaging/supervisor.conf", "/etc/dd-agent/supervisor.conf"
-  copy "datadog.conf.example", "/etc/dd-agent/datadog.conf.example"
-  copy "conf.d", "/etc/dd-agent/"
-  mkdir "/etc/dd-agent/checks.d/"
-  command "chmod 755 /etc/init.d/datadog-agent"
+  copy 'packaging/supervisor.conf', '/etc/dd-agent/supervisor.conf'
+  copy 'datadog.conf.example', '/etc/dd-agent/datadog.conf.example'
+  copy 'conf.d', '/etc/dd-agent/'
+  mkdir '/etc/dd-agent/checks.d/'
+  command 'chmod 755 /etc/init.d/datadog-agent'
 
   # Create symlinks
-  command "ln -sf /opt/datadog-agent/agent/agent.py /usr/bin/dd-agent"
-  command "ln -sf /opt/datadog-agent/agent/dogstatsd.py /usr/bin/dogstatsd"
-  command "ln -sf /opt/datadog-agent/agent/ddagent.py /usr/bin/dd-forwarder"
-  command "chmod 755 /usr/bin/dd-agent"
-  command "chmod 755 /usr/bin/dogstatsd"
-  command "chmod 755 /usr/bin/dd-forwarder"
+  command 'ln -sf /opt/datadog-agent/agent/agent.py /usr/bin/dd-agent'
+  command 'ln -sf /opt/datadog-agent/agent/dogstatsd.py /usr/bin/dogstatsd'
+  command 'ln -sf /opt/datadog-agent/agent/ddagent.py /usr/bin/dd-forwarder'
+  command 'chmod 755 /usr/bin/dd-agent'
+  command 'chmod 755 /usr/bin/dogstatsd'
+  command 'chmod 755 /usr/bin/dd-forwarder'
 end
