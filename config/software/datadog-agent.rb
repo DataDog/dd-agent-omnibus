226c48b036005aa99a76b082d79f6bc2d1553b39
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-07-28 20:22:07 -0400

    facepalm

--- config/software/datadog-agent.rb
+++ config/software/datadog-agent.rb
@@ -53,9 +53,9 @@ build do
       command 'chmod 755 /etc/init.d/datadog-agent'
 
       # Let's mark the example conf files as extra package files
-      log.info(log_key){ "Hi there, this is a dirty way of debugging" }
+      File.write('/var/cache/omnibus/pkg/logtiti', "Hi there, this is a dirty way of debugging")
       Dir.glob('conf.d/*').each do |path|
-        log.info(log_key){ path }
+        File.write('/var/cache/omnibus/pkg/logtiti', path)
         project.extra_package_file "/etc/dd-agent/#{path}"
       end
 
