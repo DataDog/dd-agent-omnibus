873f6e0e981e73681d355b93292c5dabef740954
Kevin Lu <luhkevin@gmail.com>
2016-07-08 18:48:14 -0700

    helpful comments

--- config/software/traffic-collector.rb
+++ config/software/traffic-collector.rb
@@ -14,21 +14,22 @@
 # limitations under the License.
 #
 
+# Self-explanatory: this specifies the name of the software
+# You will need to add this as a dependency in config/projects/netsil-collectors.rb. Look at the end of that file to see how we do it for traffic-collector
 name "traffic-collector"
 
-# A software can specify more than one version that is available for install
-# version("#{version}") { source url: "https://github.com/DataDog/dd-agent/archive/#{version}.tar.gz" }
-#source url: "https://s3.amazonaws.com/bin.netsil.io/rpcapd/traffic-collector.tar.gz",
-#       md5: "437e0f251868d07ebb020be1a13be32f"
-
+# This source path is copied/volume-mounted at dev-time from the AOC repo.
+# E.g. in the omnibus dockerfiles, we COPY tools/collectors/rpcapd to /root/collectors/rpcapd
 source path: "/root/collectors/rpcapd"
 
-# This is the path, inside the tarball, where the source resides
-#relative_path "traffic-collector"
-
+# These dependencies come from the repo github.com/luhkevin/omnibus-software
+# Check the files in config/software in that repo to see how these dependencies are written.
+# If you need to add custom dependencies (e.g. let's say you need a library called `libfoo`),
+# you will have to add a software definition at `config/software/libfoo` in the omnibus-software repo above.
 dependency "libpcap"
 dependency "libsodium"
 
+# Compile-time environment variables
 env = {
   "LIBRARY_PATH" => "#{install_dir}/embedded/lib",
   "CPATH" => "#{install_dir}/embedded/include",
@@ -36,11 +37,15 @@ env = {
 }
 
 build do
-  # Setup a default environment from Omnibus - you should use this Omnibus
-  # helper everywhere. It will become the default in the future.
-  #env = with_standard_compiler_flags(with_embedded_path)
+  # The #{install_dir} variable is /opt/netsil/collectors.
+  # Basically this command is making the actual user-space directory for the traffic-collector that omnibus will then package into the DEB or RPM
   mkdir "#{install_dir}/traffic-collector"
+
+  # Notice that we don't specify which directory this is running from.
+  # By default, all these commands -- e.g. `make` or the `copy` step -- are happening from the "source path:" directory defined above.
   command "make PLATFORM=linux", :env => env
+
+  # To reiterate the point about #{install_dir}, here we are copying rpcapd into the install_dir, so omnibus can package it when it generates the DEB or RPM
   copy "winpcap/wpcap/libpcap/rpcapd/rpcapd", "#{install_dir}/traffic-collector/rpcapd"
   copy "supervisord-rpcapd.sh", "#{install_dir}/traffic-collector/supervisord-rpcapd.sh"
 end
