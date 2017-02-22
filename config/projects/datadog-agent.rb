0f4a48a6d2bfe0b5ea8897e66e2e13a1127027ea
Quentin Madec <quentin.madec@datadoghq.com>
2017-02-22 15:57:08 -0500

    [mac] do not build gohai

--- config/projects/datadog-agent.rb
+++ config/projects/datadog-agent.rb
@@ -176,12 +176,12 @@ dependency 'datadogpy'
 # are built last before datadog-agent since they should always be rebuilt
 # (if put above, they would dirty the cache of the dependencies below
 # and trigger a useless rebuild of many packages)
-dependency 'datadog-gohai'
 if linux? and ohai['kernel']['machine'] == 'x86_64'
   dependency 'datadog-metro'
 end
 
 if linux?
+  dependency 'datadog-gohai'
   dependency 'datadog-trace-agent'
 end
 
