936c4b4c9670d90e9a317982225e6d478b4976c5
Greg Meyer <greg.meyer@datadoghq.com>
2017-03-08 15:29:57 -0500

    adds a 1.0.0 validator to validate manifest

--- resources/datadog-integrations/validate_manifest.rb
+++ resources/datadog-integrations/validate_manifest.rb
@@ -17,6 +17,24 @@ def validate_manifest_0_1_0(manifest_hash)
     "support",
     "version",
   ]
+  validate_manifest_loop(manifest_hash, mandatory_fields)
+end
+
+def validate_manifest_1_0_0(manifest_hash)
+  mandatory_fields = [
+    "maintainer",
+    "manifest_version",
+    "max_agent_version",
+    "min_agent_version",
+    "name",
+    "short_description",
+    "support",
+    "version",
+  ]
+  validate_manifest_loop(manifest_hash, mandatory_fields)
+end
+
+def validate_manifest_loop(manifest_hash, mandatory_fields)
   mandatory_fields.each do |field|
     raise "manifest.json needs proper fields, currently missing #{field}. Please refer to documentation" unless manifest_hash.key?(field)
   end
