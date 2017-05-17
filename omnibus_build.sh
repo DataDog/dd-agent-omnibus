a12d335f98eb7faae0dde360f2c054b7ea9d312d
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-05-17 13:49:39 -0400

    [jmxfetch] simplifying script.
    [jmxfetch] we always set 'current' version

--- omnibus_build.sh
+++ omnibus_build.sh
@@ -9,7 +9,6 @@
 PROJECT_DIR=dd-agent-omnibus
 PROJECT_NAME=datadog-agent
 LOG_LEVEL=${LOG_LEVEL:-"info"}
-JMX_VERSION=${JMX_BRANCH:-"current"}
 export AGENT_BRANCH=${AGENT_BRANCH:-"master"}
 export OMNIBUS_BRANCH=${OMNIBUS_BRANCH:-"master"}
 export OMNIBUS_SOFTWARE_BRANCH=${OMNIBUS_SOFTWARE_BRANCH:-"master"}
@@ -18,12 +17,6 @@ export INTEGRATION_CORE_BRANCH=${INTEGRATION_CORE_BRANCH:-"master"}
 
 REMOTE_AGENT_REPO_RAW="https://raw.githubusercontent.com/DataDog/dd-agent"
 LOCAL_DD_AGENT="/dd-agent-repo"
-
-unset_jmx() {
-    echo "JMX provided by dd-agent in selected branch, JMX environment variables will be ignored."
-    unset JMX_VERSION
-    unset JMX_BRANCH
-}
 
 set -e
 
@@ -48,37 +41,16 @@ if [ -n "$RPM_SIGNING_PASSPHRASE" ]; then
   gpg --import /keys/RPM-SIGNING-KEY.private
 fi
 
+# NOTE: JMX_VERSION plays no role <5.14.x (jmx bundled with agent)
 # If "current" JMX version, set it from the corresponding agent branch config.py
-if [ "$JMX_VERSION" == "current" ]; then
-  if [ -n "$LOCAL_AGENT_REPO" ]; then
-    cd $LOCAL_DD_AGENT
-    JMX_VERSION=$(git show $AGENT_BRANCH:config.py | grep 'JMX_VERSION' | cut -f2 -d'=' | tr -d ' "')
-    cd -
-  else
-    JMX_VERSION=$(curl -v $REMOTE_AGENT_REPO_RAW/$AGENT_BRANCH/config.py 2>/dev/null | grep 'JMX_VERSION' | cut -f2 -d'=' | tr -d ' "')
-  fi
-
-  if [ -n "$JMX_VERSION" ]; then
-    export JMX_VERSION=$JMX_VERSION
-  else
-    unset_jmx
-  fi
+if [ -n "$LOCAL_AGENT_REPO" ]; then
+  cd $LOCAL_DD_AGENT
+  JMX_VERSION=$(git show $AGENT_BRANCH:config.py | grep 'JMX_VERSION' | cut -f2 -d'=' | tr -d ' "')
+  cd -
 else
-  # just verify the specified repo supports distributing JMXFetch from standalone S3.
-  if [ -n "$LOCAL_AGENT_REPO" ]; then
-    cd $LOCAL_DD_AGENT
-    git show $AGENT_BRANCH:config.py | grep 'JMX_VERSION'
-    if [ $? -ne 0 ]; then
-      unset_jmx
-    fi
-    cd -
-  else
-    curl -v $REMOTE_AGENT_REPO_RAW/$AGENT_BRANCH/config.py 2>/dev/null | grep 'JMX_VERSION'
-    if [ $? -ne 0 ]; then
-      unset_jmx
-    fi
-  fi
+  JMX_VERSION=$(curl -v $REMOTE_AGENT_REPO_RAW/$AGENT_BRANCH/config.py 2>/dev/null | grep 'JMX_VERSION' | cut -f2 -d'=' | tr -d ' "')
 fi
+export JMX_VERSION=$JMX_VERSION
 
 # Install the gems we need, with stubs in bin/
 bundle update # Make sure to update to the latest version of omnibus-software
