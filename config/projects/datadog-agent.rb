44451ec50d593bc26fcee45a1866eec48be2b1fc
root <root@puppet4.infra.dev.gradwell.net>
2017-06-06 15:50:56 +0100

    Revert "move dd-agent file olny to rpms part"
    This reverts commit 8e4448d8cd515aa1d64b32aaf96fac6ebc564186.

--- config/projects/datadog-agent.rb
+++ config/projects/datadog-agent.rb
@@ -126,12 +126,6 @@ if linux?
   # SysVInit service file
   if redhat?
     extra_package_file '/etc/rc.d/init.d/datadog-agent'
-  # Just a dummy file that needs to be in the RPM package list if we don't want it to be removed
-  # during RPM upgrades. (the old files from the RPM file listthat are not in the new RPM file
-  # list will get removed, that's why we need this one here)
-    touch '/usr/bin/dd-agent'
-    extra_package_file '/usr/bin/dd-agent'
-  
   end
 
   if suse?
