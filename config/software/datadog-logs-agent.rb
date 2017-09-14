5af3847f773af7f493eb821689b590d98324a95d
Tristan Michelet <tristan@datadoghq.com>
2017-09-14 11:30:12 +0200

    d

--- config/software/datadog-logs-agent.rb
+++ config/software/datadog-logs-agent.rb
@@ -1,6 +1,8 @@
 name "datadog-logs-agent"
 always_build true
 
+puts "debug tristan"
+puts ENV['INCLUDE_LOGS_AGENT']
 include_logs_agent = ENV['INCLUDE_LOGS_AGENT']
 if include_logs_agent.nil? || include_logs_agent.empty?
     include_logs_agent = false
@@ -14,12 +16,16 @@ end
 build do
   binary = "logagent"
 
+  puts include_logs_agent
   if include_logs_agent
-      if linux?
-        url = "https://s3.amazonaws.com/public.binaries.sheepdog.datad0g.com/agent/#{logs_agent_version}/linux-amd64/#{binary}"
-        command "curl #{url} -o #{binary}"
-        command "chmod +x #{binary}"
-        command "mv #{binary} #{install_dir}/bin/logs-agent"
-      end
+    puts "including the logs-agent"
+    if linux?
+      url = "https://s3.amazonaws.com/public.binaries.sheepdog.datad0g.com/agent/#{logs_agent_version}/linux-amd64/#{binary}"
+      command "curl #{url} -o #{binary}"
+      command "chmod +x #{binary}"
+      command "mv #{binary} #{install_dir}/bin/logs-agent"
+    end
+  else
+    puts "not including the logs-agent"
   end
 end 
