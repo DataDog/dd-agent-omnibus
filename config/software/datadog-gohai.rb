9793d29ccd39c6f14cc9ac8ef3ef438c3c0f67c5
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-11-30 18:17:50 +0100

    [metro][gohai] lets try this dynamically in ruby block

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -9,44 +9,45 @@ always_build true
 
 go_version = "1.9.1"
 
-srcdir = "#{Omnibus::Config.source_dir}/#{name}"
-gopath = "#{Omnibus::Config.cache_dir}/go/src/#{name}",
-
 build do
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/LICENSE"
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/THIRD_PARTY_LICENSES.md"
 
-  godir = go_setup(go_version)
-  gobin = "#{godir}/go/bin/go"
+  block do
+    srcdir = "#{Omnibus::Config.source_dir}/#{name}"
+    gopath = "#{Omnibus::Config.cache_dir}/go/src/#{name}",
+    godir = go_setup(go_version)
+    gobin = "#{godir}/go/bin/go"
 
-  env = {
-    "GOPATH" => gopath,
-    "GOROOT" => "#{godir}/go",
-    "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
-  }
+    env = {
+      "GOPATH" => "#{gopath}",
+      "GOROOT" => "#{godir}/go",
+      "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
+    }
 
-  # Put gohai into the GOPATH
-  mkdir "#{gopath}/src/github.com/DataDog/"
-  delete "#{gopath}/src/github.com/DataDog/gohai"
-  mkdir "#{gopath}/src/github.com/DataDog/gohai"
+    # Put gohai into the GOPATH
+    mkdir "#{gopath}/src/github.com/DataDog/"
+    delete "#{gopath}/src/github.com/DataDog/gohai"
+    mkdir "#{gopath}/src/github.com/DataDog/gohai"
 
-  command "git checkout #{version} && git pull", :env => env, :cwd => srcdir 
-  copy "#{srcdir}/*", "#{gopath}/src/github.com/DataDog/gohai"
+    command "git checkout #{version} && git pull", :env => env, :cwd => srcdir 
+    copy "#{srcdir}/*", "#{gopath}/src/github.com/DataDog/gohai"
 
-  # Checkout gohai's deps
-  command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
-  command "git checkout v2.0.0", :env => env, :cwd => "#{gopath}/src/github.com/shirou/gopsutil"
-  command "#{gobin} get -u github.com/cihub/seelog", :env => env
-  command "git checkout v2.6", :env => env, :cwd => "#{gopath}/src/github.com/cihub/seelog"
-  # Windows depends on the registry, go get that.
-  if ohai["platform"] == "windows"
-    command "#{gobin} get golang.org/x/sys/windows/registry", :env => env
+    # Checkout gohai's deps
+    command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
+    command "git checkout v2.0.0", :env => env, :cwd => "#{gopath}/src/github.com/shirou/gopsutil"
+    command "#{gobin} get -u github.com/cihub/seelog", :env => env
+    command "git checkout v2.6", :env => env, :cwd => "#{gopath}/src/github.com/cihub/seelog"
+    # Windows depends on the registry, go get that.
+    if ohai["platform"] == "windows"
+      command "#{gobin} get golang.org/x/sys/windows/registry", :env => env
+    end
+    # Checkout and build gohai
+    command "#{gobin} get -d github.com/DataDog/gohai", :env => env # No need to pull latest from remote with `-u` here since the next command checks out and pulls latest
+    command "git checkout #{version} && git pull", :env => env, :cwd => "#{gopath}/src/github.com/DataDog/gohai"
+    command "cd #{gopath}/src/github.com/DataDog/gohai && #{gobin} run make.go #{gobin} && mv gohai #{install_dir}/bin/gohai", :env => env
+
+    # clean up extra go compiler
+    delete godir
   end
-  # Checkout and build gohai
-  command "#{gobin} get -d github.com/DataDog/gohai", :env => env # No need to pull latest from remote with `-u` here since the next command checks out and pulls latest
-  command "git checkout #{version} && git pull", :env => env, :cwd => "#{gopath}/src/github.com/DataDog/gohai"
-  command "cd #{gopath}/src/github.com/DataDog/gohai && #{gobin} run make.go #{gobin} && mv gohai #{install_dir}/bin/gohai", :env => env
-
-  # clean up extra go compiler
-  delete godir
 end
--- config/software/datadog-metro.rb
+++ config/software/datadog-metro.rb
@@ -11,43 +11,44 @@ always_build true
 
 go_version = "1.9.1"
 
-srcdir = "#{Omnibus::Config.source_dir}/#{name}"
-gopath = "#{Omnibus::Config.cache_dir}/go/src/#{name}",
-
 build do
   ship_license "https://raw.githubusercontent.com/DataDog/go-metro/master/LICENSE"
   ship_license "https://raw.githubusercontent.com/DataDog/go-metro/master/THIRD_PARTY_LICENSES.md"
 
-  godir = go_setup(go_version)
-  gobin = "#{godir}/go/bin/go"
+  block do
+    srcdir = "#{Omnibus::Config.source_dir}/#{name}"
+    gopath = "#{Omnibus::Config.cache_dir}/go/src/#{name}",
+    godir = go_setup(go_version)
+    gobin = "#{godir}/go/bin/go"
 
-  env = {
-    "GOPATH" => gopath,
-    "GOROOT" => "#{godir}/go",
-    "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
-  }
+    env = {
+      "GOPATH" => gopath,
+      "GOROOT" => "#{godir}/go",
+      "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
+    }
 
-  # Put go-metro into the GOPATH
-  mkdir "#{gopath}/src/github.com/DataDog/"
-  delete "#{gopath}/src/github.com/DataDog/go-metro"
-  mkdir "#{gopath}/src/github.com/DataDog/go-metro"
+    # Put go-metro into the GOPATH
+    mkdir "#{gopath}/src/github.com/DataDog/"
+    delete "#{gopath}/src/github.com/DataDog/go-metro"
+    mkdir "#{gopath}/src/github.com/DataDog/go-metro"
 
-  command "git checkout #{version} && git pull", :env => env, :cwd => srcdir
-  copy "#{srcdir}/*", "#{gopath}/src/github.com/DataDog/go-metro"
+    command "git checkout #{version} && git pull", :env => env, :cwd => srcdir
+    copy "#{srcdir}/*", "#{gopath}/src/github.com/DataDog/go-metro"
 
-  command "mkdir -p #{gopath}/src/github.com/DataDog", :env => env
-  command "#{gobin} get -v -d github.com/DataDog/go-metro", :env => env, :cwd => "#{gopath}"
-  command "git checkout #{version} && git pull", :env => env, :cwd => "#{gopath}/src/github.com/DataDog/go-metro"
-  command "#{gobin} get -v -d github.com/cihub/seelog", :env => env, :cwd => "#{gopath}"
-  command "#{gobin} get -v -d github.com/google/gopacket", :env => env, :cwd => "#{gopath}"
-  command "#{gobin} get -v -d github.com/DataDog/datadog-go/statsd", :env => env, :cwd => "#{gopath}"
-  command "#{gobin} get -v -d gopkg.in/tomb.v2", :env => env, :cwd => "#{gopath}"
-  command "#{gobin} get -v -d gopkg.in/yaml.v2", :env => env, :cwd => "#{gopath}"
-  patch :source => "libpcap-static-link.patch", :plevel => 1,
-        :acceptable_output => "Reversed (or previously applied) patch detected",
-        :target => "/var/cache/omnibus/src/datadog-metro/src/github.com/google/gopacket/pcap/pcap.go"
-  command "#{gobin} build -o #{install_dir}/bin/go-metro github.com/DataDog/go-metro", :env => env, :cwd => "#{gopath}"
+    command "mkdir -p #{gopath}/src/github.com/DataDog", :env => env
+    command "#{gobin} get -v -d github.com/DataDog/go-metro", :env => env, :cwd => "#{gopath}"
+    command "git checkout #{version} && git pull", :env => env, :cwd => "#{gopath}/src/github.com/DataDog/go-metro"
+    command "#{gobin} get -v -d github.com/cihub/seelog", :env => env, :cwd => "#{gopath}"
+    command "#{gobin} get -v -d github.com/google/gopacket", :env => env, :cwd => "#{gopath}"
+    command "#{gobin} get -v -d github.com/DataDog/datadog-go/statsd", :env => env, :cwd => "#{gopath}"
+    command "#{gobin} get -v -d gopkg.in/tomb.v2", :env => env, :cwd => "#{gopath}"
+    command "#{gobin} get -v -d gopkg.in/yaml.v2", :env => env, :cwd => "#{gopath}"
+    patch :source => "libpcap-static-link.patch", :plevel => 1,
+          :acceptable_output => "Reversed (or previously applied) patch detected",
+          :target => "/var/cache/omnibus/src/datadog-metro/src/github.com/google/gopacket/pcap/pcap.go"
+    command "#{gobin} build -o #{install_dir}/bin/go-metro github.com/DataDog/go-metro", :env => env, :cwd => "#{gopath}"
 
-  # clean up extra go compiler
-  delete godir
+    # clean up extra go compiler
+    delete godir
+  end
 end
