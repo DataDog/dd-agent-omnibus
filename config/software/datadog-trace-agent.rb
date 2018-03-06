3b3eb6a135a96c49a1d4d8b9fcf8ffae71c3adab
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2018-03-06 15:02:30 +0100

    config/software: update datadog-trace-agent build steps and bump to (#235)
    go1.9.4

--- config/software/datadog-trace-agent.rb
+++ config/software/datadog-trace-agent.rb
@@ -21,7 +21,7 @@ dd_agent_version = ENV['AGENT_VERSION']
 
 if windows?
   trace_agent_bin = "trace-agent.exe"
-  gourl = "https://storage.googleapis.com/golang/go1.8.1.windows-amd64.zip"
+  gourl = "https://storage.googleapis.com/golang/go1.9.4.windows-amd64.zip"
   goout = "go.zip"
   godir = "c:/go18"
   gobin = "#{godir}/go/bin/go"
@@ -33,7 +33,7 @@ if windows?
 
 else
   trace_agent_bin = "trace-agent"
-  gourl = "https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz"
+  gourl = "https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz"
   goout = "go.tar.gz"
   godir = "/usr/local/go18"
   gobin = "#{godir}/go/bin/go"
@@ -49,7 +49,7 @@ env = {
   "GOPATH" => gopath,
   "GOROOT" => "#{godir}/go",
   "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
-  "TRACE_AGENT_VERSION" => dd_agent_version, # used by gorake.rb in the trace-agent
+  "TRACE_AGENT_VERSION" => dd_agent_version, # used by 'make' in the trace-agent
   "TRACE_AGENT_ADD_BUILD_VARS" => trace_agent_add_build_vars.to_s(),
 }
 
@@ -75,24 +75,18 @@ build do
    mkdir "#{gopath}/src/github.com/DataDog/datadog-trace-agent"
    move "#{agent_source_dir}/*", "#{gopath}/src/github.com/DataDog/datadog-trace-agent"
 
-   # Checkout datadog-trace-agent's build dependencies
-   command "#{gobin} get -d github.com/Masterminds/glide", :env => env, :cwd => agent_cache_dir
+   if windows?
+     # build windows resources
+     command "make windows", :env => env, :cwd => agent_cache_dir
+   end
 
-   # Pin build deps to known versions
-   command "git reset --hard v0.12.3", :env => env, :cwd => glide_cache_dir
-   command "#{gobin} install github.com/Masterminds/glide", :env => env, :cwd => glide_cache_dir
+   # build datadog-trace-agent
+   command "make install", :env => env, :cwd => agent_cache_dir
 
-   # Build datadog-trace-agent
-   command "#{gopath}/bin/glide install", :env => env, :cwd => agent_cache_dir
    if rhel? # temporary workaround for RHEL 5 build issue with the regular `build -a` command
-     command "rake install", :env => env, :cwd => agent_cache_dir
      command "mv $GOPATH/bin/#{trace_agent_bin} #{install_dir}/bin/#{trace_agent_bin}", :env => env, :cwd => agent_cache_dir
-   elsif windows?
-    command "rake build windres=true", :env => env, :cwd => agent_cache_dir
-    command "mv ./#{trace_agent_bin} #{install_dir}/bin/#{trace_agent_bin}", :env => env, :cwd => agent_cache_dir
    else
-     command "rake build", :env => env, :cwd => agent_cache_dir
-     command "mv ./#{trace_agent_bin} #{install_dir}/bin/#{trace_agent_bin}", :env => env, :cwd => agent_cache_dir
+     command "mv #{gopath}/bin/#{trace_agent_bin} #{install_dir}/bin/#{trace_agent_bin}", :env => env, :cwd => agent_cache_dir
    end
    # clean up extra go compiler
    delete godir
