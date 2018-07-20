85fa081def4cc0b997dd31bb80b51ef7a0902655
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-07-20 19:09:06 -0400

    Address comments and uninstall pip-tools

--- config/software/datadog-agent-integrations.rb
+++ config/software/datadog-agent-integrations.rb
@@ -9,6 +9,7 @@ relative_path 'integrations-core'
 
 PIPTOOLS_VERSION = "2.0.2"
 WHEELS_VERSION = "0.30.0"
+UNINSTALL_PIPTOOLS_DEPS = ['first', 'click', 'pip-tools']
 
 # The only integrations that will be packaged with the agent
 # are the ones that are officiallly supported.
@@ -60,34 +61,40 @@ build do
     # FIX THIS these dependencies have to be grabbed from somewhere
     all_reqs_file.puts "wheel==#{WHEELS_VERSION} --hash=sha256:e721e53864f084f956f40f96124a74da0631ac13fbbd1ba99e8e2b5e9cafdf64"\
         " --hash=sha256:9515fe0a94e823fd90b08d22de45d7bde57c90edce705b22f5e1ecf7e1b653c8"
-    all_reqs_file.puts "pip-tools==#{PIPTOOLS_VERSION} --hash=sha256:90bbe6731a6a34d339bf14d90cf2892475386c7d06c458208191ac9992110e0a"
     all_reqs_file.close
 
     # Install all the requirements
     if windows?
+      pip "install pip-tools==#{PIPTOOLS_VERSION}"
       pip "install -r #{project_dir}/check_requirements.txt"
     else
       build_env = {
         "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
         "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
       }
+      pip "install pip-tools==#{PIPTOOLS_VERSION}", :env => build_env
       pip "install -r #{project_dir}/check_requirements.txt", :env => build_env
     end
 
     # Set frozen requirements
     pip "freeze > #{install_dir}/agent_requirements.txt"
 
-
     # Windows pip workaround to support globs
     python_bin = "\"#{windows_safe_path(install_dir)}\\embedded\\python.exe\""
     python_pip_no_deps = "pip install --no-deps #{windows_safe_path(project_dir)}"
     python_pip_reqs = "pip install -c #{windows_safe_path(install_dir)}\\agent_requirements.txt --require-hashes -r #{windows_safe_path(project_dir)}"
-
+    python_pip_uninstall = "pip uninstall -y"
 
     # Install the static environment requirements that the Agent and all checks will use
     if windows?
       command("#{python_bin} -m #{python_pip_no_deps}\\datadog_checks_base")
-      command("#{python_bin} -m piptools compile --generate-hashes --output-file #{windows_safe_path(project_dir)}\\static_requirements.txt #{windows_safe_path(project_dir)}\\datadog_checks_base\\agent_requirements.in")
+      command("#{python_bin} -m piptools compile --generate-hashes --output-file #{windows_safe_path(project_dir)}\\static_requirements.txt #{windows_safe_path(project_dir)}\\datadog_checks_base\\datadog_checks\\data\\agent_requirements.in")
+
+      # Uninstall the deps that pip-compile installs so we don't include them in the final artifact
+      for dep in UNINSTALL_PIPTOOLS_DEPS
+        command("#{python_bin} -m #{python_pip_uninstall} #{dep}")
+      end
+
       command("#{python_bin} -m #{python_pip_reqs}\\static_requirements.txt")
     else
       build_env = {
@@ -95,10 +102,16 @@ build do
         "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
       }
       pip "install --no-deps .", :env => build_env, :cwd => "#{project_dir}/datadog_checks_base"
-      command("#{install_dir}/embedded/bin/python -m piptools compile --generate-hashes --output-file #{project_dir}/static_requirements.txt #{project_dir}/datadog_checks_base/agent_requirements.in")
+      command("#{install_dir}/embedded/bin/python -m piptools compile --generate-hashes --output-file #{project_dir}/static_requirements.txt #{project_dir}/datadog_checks_base/datadog_checks/data/agent_requirements.in")
+
+      # Uninstall the deps that pip-compile installs so we don't include them in the final artifact
+      for dep in UNINSTALL_PIPTOOLS_DEPS
+        pip "uninstall -y #{dep}"
+      end
+      
       pip "install -c #{install_dir}/agent_requirements.txt --require-hashes -r #{project_dir}/static_requirements.txt"
     end
-    
+
     # loop through checks and install each without their dependencies
     # we rely on a static Agent environment that was built above. 
     checks.each do |check|
