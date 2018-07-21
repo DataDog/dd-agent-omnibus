c514c2655143b3b15e193c317847a0a48fd590d5
Derek Brown <derek.brown@datadoghq.com>
2018-07-20 20:36:17 -0700

    (temporarily) change trace agent branch for debugging

--- config/software/datadog-trace-agent.rb
+++ config/software/datadog-trace-agent.rb
@@ -24,12 +24,20 @@ if windows?
   gourl = "https://storage.googleapis.com/golang/go1.10.3.windows-amd64.zip"
   goout = "go.zip"
   godir = "c:/go110"
-  gobin = "#{godir}/go/bin/go"
-  gopath = "#{Omnibus::Config.cache_dir}/src/#{name}"
+  godirwin = "c:\\go110"
+  gobin = "c:\\go110\\go\\bin\\go"
+  gopath = "#{Omnibus::Config.cache_dir}\\src\\#{name}"
 
   agent_source_dir = "#{Omnibus::Config.source_dir}/datadog-trace-agent"
   glide_cache_dir = "#{gopath}/src/github.com/Masterminds/glide"
   agent_cache_dir = "#{gopath}/src/github.com/DataDog/datadog-trace-agent"
+  env = {
+    "GOPATH" => gopath,
+    "GOROOT" => "#{godirwin}\\go",
+    "PATH" => "#{godirwin}\\go\\bin;#{ENV["PATH"]}",
+    "TRACE_AGENT_VERSION" => dd_agent_version, # used by 'make' in the trace-agent
+    "TRACE_AGENT_ADD_BUILD_VARS" => trace_agent_add_build_vars.to_s(),
+  }
 
 else
   trace_agent_bin = "trace-agent"
@@ -42,16 +50,18 @@ else
   agent_source_dir = "#{Omnibus::Config.source_dir}/datadog-trace-agent"
   glide_cache_dir = "#{gopath}/src/github.com/Masterminds/glide"
   agent_cache_dir = "#{gopath}/src/github.com/DataDog/datadog-trace-agent"
+ 
+  env = {
+    "GOPATH" => gopath,
+    "GOROOT" => "#{godir}/go",
+    "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
+    "TRACE_AGENT_VERSION" => dd_agent_version, # used by 'make' in the trace-agent
+    "TRACE_AGENT_ADD_BUILD_VARS" => trace_agent_add_build_vars.to_s(),
+  }
 
 end
 
-env = {
-  "GOPATH" => gopath,
-  "GOROOT" => "#{godir}/go",
-  "PATH" => "#{godir}/go/bin:#{ENV["PATH"]}",
-  "TRACE_AGENT_VERSION" => dd_agent_version, # used by 'make' in the trace-agent
-  "TRACE_AGENT_ADD_BUILD_VARS" => trace_agent_add_build_vars.to_s(),
-}
+
 
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/LICENSE"
