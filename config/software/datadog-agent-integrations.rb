f9a19b250eeebbd9735209fe971c08df4958c0fa
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-07-23 19:57:57 -0400

    Clean up integration core deps and yaml files (#268)
    * Use new conf.yaml directories from integrations-core
    
    * Clean up agent-integration requirements
    
    * Add comments and remove wheel dependency
    
    * Clean blacklist
    
    * Add section to release to test build
    
    * Fix blacklist...ruby
    
    * Update trace version in release.json
    
    * Compile requirements here
    
    * Add comment
    
    * Address review
    
    * fix missing paren
    
    * install pip-tools
    
    * try
    
    * Fix paren mismatch
    
    * Fix paren mismatch
    
    * use python path directly
    
    * But like... for real this time... :pineapple:
    
    * Address comments
    
    * Address comments and uninstall pip-tools
    
    * Make sure freeze constraints doesn't include pip-tools
    
    * Add back release.json for testing
    
    * Remove test section from release.json
    
    * address review
    
    * incoporate feedback to only uninstall deps not required by the Agent
    
    * Fix path
    
    * Fix build
    
    * Fix for loop end
    
    * Invoke pip properly on windows

--- config/software/datadog-agent-integrations.rb
+++ config/software/datadog-agent-integrations.rb
@@ -7,6 +7,10 @@ dependency 'datadog-agent'
 
 relative_path 'integrations-core'
 
+PIPTOOLS_VERSION = "2.0.2"
+WHEELS_VERSION = "0.30.0"
+UNINSTALL_PIPTOOLS_DEPS = ['click', 'first', 'pip-tools']
+
 # The only integrations that will be packaged with the agent
 # are the ones that are officiallly supported.
 local_integrations_core_repo = ENV['LOCAL_INTEGRATIONS_CORE_REPO']
@@ -24,8 +28,10 @@ else
   default_version integrations_core_branch
 end
 
+# Skip installing checks that aren't consumer facing
 blacklist = [
   'datadog_checks_base',  # namespacing package for wheels (NOT AN INTEGRATION)
+  'datadog_checks_dev',   # developer tooling for working on integrations (NOT AN INTEGRATION)
 ]
 
 python_lib_path = File.join(install_dir, "embedded", "lib", "python2.7", "site-packages")
@@ -53,42 +59,64 @@ build do
     all_reqs_file = File.open("#{project_dir}/check_requirements.txt", 'w+')
     # Manually add "core" dependencies that are not listed in the checks requirements
     # FIX THIS these dependencies have to be grabbed from somewhere
-    all_reqs_file.puts "wheel==0.30.0 --hash=sha256:e721e53864f084f956f40f96124a74da0631ac13fbbd1ba99e8e2b5e9cafdf64"\
+    all_reqs_file.puts "wheel==#{WHEELS_VERSION} --hash=sha256:e721e53864f084f956f40f96124a74da0631ac13fbbd1ba99e8e2b5e9cafdf64"\
         " --hash=sha256:9515fe0a94e823fd90b08d22de45d7bde57c90edce705b22f5e1ecf7e1b653c8"
-
     all_reqs_file.close
+
+    # Set frozen requirements
+    pip "freeze > #{install_dir}/agent_requirements.txt"
 
     # Install all the requirements
     if windows?
+      pip "install pip-tools==#{PIPTOOLS_VERSION}"
       pip "install -r #{project_dir}/check_requirements.txt"
     else
       build_env = {
         "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
         "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
       }
+      pip "install pip-tools==#{PIPTOOLS_VERSION}", :env => build_env
       pip "install -r #{project_dir}/check_requirements.txt", :env => build_env
     end
 
-    # Set frozen requirements
-    pip "freeze > #{install_dir}/agent_requirements.txt"
-
     # Windows pip workaround to support globs
     python_bin = "\"#{windows_safe_path(install_dir)}\\embedded\\python.exe\""
-    python_pip = "pip install -c #{windows_safe_path(install_dir)}\\agent_requirements.txt #{windows_safe_path(project_dir)}"
+    python_pip_no_deps = "pip install --no-deps #{windows_safe_path(project_dir)}"
+    python_pip_reqs = "pip install -c #{windows_safe_path(install_dir)}\\agent_requirements.txt --require-hashes -r"
+    python_pip_uninstall = "pip uninstall -y"
 
+    # Install the static environment requirements that the Agent and all checks will use
     if windows?
-      command("#{python_bin} -m #{python_pip}\\datadog_checks_base")
+      command("#{python_bin} -m #{python_pip_no_deps}\\datadog_checks_base")
+      command("#{python_bin} -m piptools compile --generate-hashes --output-file #{windows_safe_path(project_dir)}\\static_requirements.txt #{windows_safe_path(project_dir)}\\datadog_checks_base\\datadog_checks\\data\\agent_requirements.in")
+
+      # Uninstall the deps that pip-compile installs so we don't include them in the final artifact
+      for dep in UNINSTALL_PIPTOOLS_DEPS
+        command("#{python_bin} -m #{python_pip_uninstall} #{dep}")
+      end
+
+      command("#{python_bin} -m #{python_pip_reqs} #{windows_safe_path(project_dir)}\\static_requirements.txt")
     else
       build_env = {
         "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
         "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
       }
-      pip "wheel --no-deps .", :env => build_env, :cwd => "#{project_dir}/datadog_checks_base"
-      pip "install -c #{install_dir}/agent_requirements.txt *.whl", :env => build_env, :cwd => "#{project_dir}/datadog_checks_base"
+      pip "install --no-deps .", :env => build_env, :cwd => "#{project_dir}/datadog_checks_base"
+      command("#{install_dir}/embedded/bin/python -m piptools compile --generate-hashes --output-file #{project_dir}/static_requirements.txt #{project_dir}/datadog_checks_base/datadog_checks/data/agent_requirements.in")
+
+      # Uninstall the deps that pip-compile installs so we don't include them in the final artifact
+      for dep in UNINSTALL_PIPTOOLS_DEPS
+        pip "uninstall -y #{dep}"
+      end
+      
+      pip "install -c #{install_dir}/agent_requirements.txt --require-hashes -r #{project_dir}/static_requirements.txt"
     end
 
-    # loop through them
+    # loop through checks and install each without their dependencies
+    # we rely on a static Agent environment that was built above. 
     checks.each do |check|
+      next if blacklist.include?(check)
+
       # Only use the parts of the filename we need
       check.slice! "#{project_dir}/"
       check.slice! "/"
@@ -111,31 +139,34 @@ build do
       end
 
       # Copy the check config to the conf directories
-      if File.exists? "#{project_dir}/#{check}/conf.yaml.example"
-        copy "#{project_dir}/#{check}/conf.yaml.example", "#{conf_directory}/#{check}.yaml.example"
+      if File.exists? "#{project_dir}/#{check}/datadog_checks/#{check}/data/conf.yaml.example"
+        copy "#{project_dir}/#{check}/datadog_checks/#{check}/data/conf.yaml.example", "#{conf_directory}/#{check}.yaml.example"
       end
       # Copy the default config, if it exists
-      if File.exists? "#{project_dir}/#{check}/conf.yaml.default"
-        copy "#{project_dir}/#{check}/conf.yaml.default", "#{conf_directory}/#{check}.yaml.default"
+      if File.exists? "#{project_dir}/#{check}/datadog_checks/#{check}/data/conf.yaml.default"
+        copy "#{project_dir}/#{check}/datadog_checks/#{check}/data/conf.yaml.default", "#{conf_directory}/#{check}.yaml.default"
       end
 
       # We don't have auto_conf on windows yet
       unless windows?
-        if File.exists? "#{project_dir}/#{check}/auto_conf.yaml"
-          copy "#{project_dir}/#{check}/auto_conf.yaml", "#{conf_directory}/auto_conf/#{check}.yaml"
+        if File.exists? "#{project_dir}/#{check}/datadog_checks/#{check}/data/auto_conf.yaml"
+          copy "#{project_dir}/#{check}/datadog_checks/#{check}/data/auto_conf.yaml", "#{conf_directory}/auto_conf/#{check}.yaml"
         end
       end
 
       File.file?("#{project_dir}/#{check}/setup.py") || next
+
+      # Installing each integration with no deps because each integration depends
+      # only datadog_checks_base. All integrations-core requirements are now in requirements.in in the repo
+      # We won't install deps here to help ensure that no new dependencies sneak into the setup.py during the PR review process. 
       if windows?
-        command("#{python_bin} -m #{python_pip}\\#{check}")
+        command("#{python_bin} -m #{python_pip_no_deps}\\#{check}")
       else
         build_env = {
           "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
           "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
         }
-        pip "wheel --no-deps .", :env => build_env, :cwd => "#{project_dir}/#{check}"
-        pip "install -c #{install_dir}/agent_requirements.txt *.whl", :env => build_env, :cwd => "#{project_dir}/#{check}"
+        pip "install --no-deps .", :env => build_env, :cwd => "#{project_dir}/#{check}"
       end
     end
     if windows?
